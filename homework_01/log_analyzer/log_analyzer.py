86f0f210df8a8f844ccc66d07b73cd63b7defbbd
unknown <you@example.com>
2023-06-29 23:30:44 +0300

    added solution for log_analyzer

--- homework_01/log_analyzer/log_analyzer.py
+++ homework_01/log_analyzer/log_analyzer.py
@@ -7,15 +7,184 @@
 #                     '"$http_user_agent" "$http_x_forwarded_for" "$http_X_REQUEST_ID" "$http_X_RB_USER" '
 #                     '$request_time';
 
+import os
+import glob
+import gzip
+import re
+import json
+import argparse
+import sys
+from datetime import date
+
+
+LOG_DATE = re.compile(r'nginx-access-ui.log-(?P<full_date>(?P<year>\d{4})(?P<month>\d{2})(?P<day>\d{2}))')
+URL_RT = re.compile(r'\"\w+ (?P<url>(.*?)) HTTP.* (?P<rt>[0-9.]+)$')
+MARKER = '$table_json'
+
 config = {
     "REPORT_SIZE": 1000,
     "REPORT_DIR": "./reports",
-    "LOG_DIR": "./log"
+    "LOG_DIR": "./log",
+    "TEMPLATE": "./report.html",
+    "DIGITS": 3
 }
 
+def get_last_log_file():
+    list_of_files = glob.glob(os.path.join(config['LOG_DIR'], '*'))
+    list_of_logs_filtered = (el for el in list_of_files if LOG_DATE.search(el))
+    list_of_logs = ((LOG_DATE.search(el).group('full_date'), el)
+                    for el in list_of_logs_filtered)
+    try:
+        last = max(list_of_logs, key=lambda x: x[0])
+        return last[1]
+    except ValueError:
+        return None
 
-def main():
-    pass
+
+def get_open_func(filename):
+    _, ext = os.path.splitext(filename)
+    if ext == '.gz':
+        return gzip.open
+    else:
+        return open
+
+
+def get_url_rt(line):
+    m = URL_RT.search(line.decode())
+    if m:
+        return m.group('url'), float(m.group('rt'))
+    return None, 0
+
+
+def parse_log(filename):
+    urls = {}
+    total_count = 0
+    total_time = 0
+    open_func = get_open_func(filename)
+    with open_func(filename, mode='r') as log:
+        for line in log:
+            line = line.strip()
+            url, rt = get_url_rt(line)
+            total_count += 1
+            total_time += rt
+            if url not in urls:
+                urls[url] = [rt]
+            else:
+                urls[url].append(rt)
+    # print(urls)
+    return total_count, total_time, urls
+
+
+def process_data(data):
+    ndigits = config['DIGITS']
+    total_count, total_time, urls = data
+    result = []
+    for url in urls:
+        count = len(urls[url])
+        count_perc = round(100 * float(count) / total_count, ndigits)
+        time_avg = round(sum(urls[url]) / count, ndigits)
+        time_max = round(max(urls[url]), ndigits)
+        time_med = round(median(sorted(urls[url])), ndigits)
+        time_sum = round(sum(urls[url]), ndigits)
+        time_perc = round(100 * time_sum / total_time, ndigits)
+        result.append({
+            "url": url,
+            "count": count,
+            "count_perc": count_perc,
+            "time_avg": time_avg,
+            "time_max": time_max,
+            "time_med": time_med,
+            "time_perc": time_perc,
+            "time_sum": time_sum,
+        })
+    result.sort(key=lambda x: x['time_sum'], reverse=True)
+    # print(result)
+    return result
+
+
+def bytes_to_string(obj):
+    if isinstance(obj, bytes):
+        return obj.decode('utf-8')
+    raise TypeError('Object of type %s is not JSON serializable' % type(obj).__name__)
+
+
+def save_to_html(filename, data):
+    with open(config['TEMPLATE'], mode='r') as template:
+        with open(filename, mode='w') as report:
+            for line in template:
+                if MARKER in line:
+                    report.write(line.replace(MARKER, json.dumps(data, default=bytes_to_string)))
+                else:
+                    report.write(line)
+
+
+def save_to_json(filename, data):
+    with open(filename, mode='w') as report:
+        report.write(json.dumps(data))
+
+
+def median(values):
+    if len(values) % 2 == 0:
+        return (values[(len(values) // 2) - 1] + values[len(values) // 2]) // 2.0
+
+    elif len(values) % 2 != 0:
+        return values[int((len(values) // 2))]
+
+
+def gen_report_name(log_filename, report_format):
+    """Generate report name."""
+    m = LOG_DATE.search(log_filename)
+    if m:
+        report_filename = 'report-{0}.{1}.{2}.{3}'.format(m.group('year'),
+                                                          m.group('month'),
+                                                          m.group('day'),
+                                                          report_format)
+        return os.path.join(config['REPORT_DIR'], report_filename)
+    else:
+        return None
+
+
+def get_report_formatters():
+    return {
+        'json': save_to_json,
+        'html': save_to_html
+    }
+
+
+def main(log, report, formatter):
+    data = parse_log(log)
+    result = process_data(data)
+    formatter(report, result)
+    # print(data)
+    # print(result)
+
 
 if __name__ == "__main__":
-    main()
+    """Log file must be in format nginx-access-ui.log-YYYYMMDD[.gz]"""
+    parser = argparse.ArgumentParser(description='Parse web server logs.')
+    parser.add_argument('--log_path', dest='log_path', help='path to the file')
+    parser.add_argument('--report_format', dest='report_format', default='html', choices=['html', 'json'],
+                        help='report format')
+    parser.add_argument('--report_size', dest='report_size', default=config['REPORT_SIZE'], help='report size in lines')
+    parsed_args = parser.parse_args()
+    formatter_func = get_report_formatters()[parsed_args.report_format]
+
+    last_log = get_last_log_file()
+
+    if not last_log:
+        sys.stderr.write('Can not find last log file\n')
+        sys.stderr.flush()
+        sys.exit(1)
+
+    report_filename = gen_report_name(last_log, parsed_args.report_format)
+
+    if not report_filename:
+        sys.stderr.write('Can not parse date from {0} filename.\n'.format(last_log))
+        sys.stderr.flush()
+        sys.exit(1)
+
+    main(last_log, report_filename, formatter_func)
+
+    # print(last_log)
+    # print(process_data)
+    # print(parse_log('./log\\nginx-access-ui.log-20170630'))
---
+++ homework_01/log_analyzer/test_log_analyzer.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+import log_analyzer
+
+
+class LogAnalyzerTests(unittest.TestCase):
+
+    def test_parse_log(self):
+        line_1 = '1.196.116.32 -  - [29/Jun/2017:03:50:22 +0300]\
+                  "GET /api/v2/banner/25019354 HTTP/1.1" 200 927 "-"\
+                  "Lynx/2.8.8dev.9 libwww-FM/2.14 SSL-MM/1.4.1 GNUTLS/2.10.5"\
+                  "-" "1498697422-2190034393-4708-9752759" "dc7161be3" 0.390'
+
+        self.assertEqual(log_analyzer.get_url_rt(line_1.encode()),
+                         ('/api/v2/banner/25019354', 0.390))
+
+    def test_median(self):
+        # медиана для четного количества
+        self.assertEqual(log_analyzer.median([1, 2, 3, 4, 5]), 3)
+        # медиана для нечетного количества
+        self.assertEqual(log_analyzer.median([1, 2, 4, 5]), 3)
+        # медиана для одного значения
+        self.assertEqual(log_analyzer.median([1]), 1)
+
+    def test_process_data(self):
+        log_list = [
+            1,
+            3,
+            ({
+                '/api/v2/banner/25019354': [0.390],
+
+            }),
+
+            ]
+        result = [
+            {
+                "url": '/api/v2/banner/25019354',
+                "count": 1,
+                "count_perc": 100.0,
+                "time_avg": 0.390,
+                "time_max": 0.390,
+                "time_med": 0.390,
+                "time_perc": 13.0,
+                "time_sum": 0.390,
+            }
+        ]
+
+        self.assertEqual(log_analyzer.process_data(log_list), result)
+
+
+if __name__ == "__main__":
+    unittest.main()
